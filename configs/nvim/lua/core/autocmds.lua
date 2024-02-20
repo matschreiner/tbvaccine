5aeada166580f7a3ec6808de855f57f89e945251
Mathias Schreiner <matschreiner@gmail.com>
2024-02-21 00:32:17 +0100

    enable cursor in active window

--- configs/nvim/lua/core/autocmds.lua
+++ configs/nvim/lua/core/autocmds.lua
@@ -1,13 +1,12 @@
+-- Enable cursorline in active window
 local cursorLineGroup = vim.api.nvim_create_augroup('AddCursorLine', { clear = true })
 
--- Enable cursorline in active window
 vim.api.nvim_create_autocmd({'BufEnter', 'WinEnter'}, {
   pattern = '*',
   command = 'setlocal cursorline',
   group = cursorLineGroup,
 })
 
--- Disable cursorline when leaving the buffer/window
 vim.api.nvim_create_autocmd({'BufLeave', 'WinLeave'}, {
   pattern = '*',
   command = 'setlocal nocursorline',
