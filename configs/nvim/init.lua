fb2c3be47cb97a0243deb9a88de8e21d04eca136
Mathias Schreiner <matschreiner@gmail.com>
2024-02-21 00:31:16 +0100

    require autocommands

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,6 +1,11 @@
 require('core.lazy')
 require('core.keymaps')
-require('plugins/nvim-tree')
+require('core.options')
+require('core.autocmds')
+require('plugins.nvim-tree')
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
 
 
 
