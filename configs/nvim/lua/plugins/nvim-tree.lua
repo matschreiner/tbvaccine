7d88f73316d6a897ba5d8e4ca4cabbbd1a3d0436
Mathias Schreiner <matschreiner@gmail.com>
2024-02-21 16:25:34 +0100

    use nvim-tree

--- configs/nvim/lua/plugins/nvim-tree.lua
+++ configs/nvim/lua/plugins/nvim-tree.lua
@@ -1,1 +1,10 @@
-require("nvim-tree").setup()
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    vim.opt.termguicolors = true
+    require("nvim-tree").setup()
+  end,
+}
