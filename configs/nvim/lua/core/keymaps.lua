fc34036d6d0bd1c8d569ed91f01c0c427a9f2d89
Mathias Schreiner <matschreiner@gmail.com>
2024-02-21 16:55:13 +0100

    reconfiguring keymaps

--- configs/nvim/lua/core/keymaps.lua
+++ configs/nvim/lua/core/keymaps.lua
@@ -1,29 +1,41 @@
-local function map(modes, lhs, rhs)
-    local options = { noremap=true, silent=true }
-    for i = 1, #modes do
-        local mode = modes:sub(i, i)
-	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-    end
-end
+
+vim.g.mapleader = ','
+
+local keymap = vim.keymap
+
 
 -- Escape
-map('i', 'jk', '<ESC>')
-map('i', 'kj', '<ESC>')
+keymap.set('i', 'jk', '<ESC>')
+keymap.set('i', 'kj', '<ESC>')
+keymap.set('i', 'Kj', '<ESC>')
+keymap.set('i', 'kJ', '<ESC>')
+keymap.set('i', 'KJ', '<ESC>')
 
--- Turbomove
-map('vn', 'J', '5j')
-map('vn', 'K', '5k')
-map('vn', 'L', '5l')
-map('vn', 'H', '5h')
+-- Turbomove 
+keymap.set('n', 'J', '5j')
+keymap.set('n', 'K', '5k')
+keymap.set('n', 'L', '5l')
+keymap.set('n', 'H', '5h')
+-- 
+keymap.set('v', 'J', '5j')
+keymap.set('v', 'K', '5k')
+keymap.set('v', 'L', '5l')
+keymap.set('v', 'H', '5h')
 
 -- Navigate between windows
-map('n', '<C-J>', '<C-W><C-J>')
-map('n', '<C-K>', '<C-W><C-K>')
-map('n', '<C-L>', '<C-W><C-L>')
-map('n', '<C-H>', '<C-W><C-H>')
+keymap.set('n', '<C-J>', '<C-W><C-J>')
+keymap.set('n', '<C-K>', '<C-W><C-K>')
+keymap.set('n', '<C-L>', '<C-W><C-L>')
+keymap.set('n', '<C-H>', '<C-W><C-H>')
 
--- NvimTree
-map('n', '<C-n>', ':NvimTreeToggle<CR>') 
+-- Window Management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-
-
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
