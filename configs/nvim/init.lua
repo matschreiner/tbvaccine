db88e25e17e0fd9414a6cc5ddc0437b23d5d1d1f
Mathias Schreiner <matschreiner@gmail.com>
2024-02-21 16:25:00 +0100

    don't initialize things here

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -2,10 +2,3 @@ require('core.lazy')
 require('core.keymaps')
 require('core.options')
 require('core.autocmds')
-require('plugins.nvim-tree')
-
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
-
-
-
