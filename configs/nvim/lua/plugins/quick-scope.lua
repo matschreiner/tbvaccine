5a65eca1be87994c1ccca3fb7a407d725e2652ac
Mathias Schreiner <matschreiner@gmail.com>
2024-02-22 18:44:34 +0100

    stylua

--- configs/nvim/lua/plugins/quick-scope.lua
+++ configs/nvim/lua/plugins/quick-scope.lua
@@ -2,10 +2,10 @@ return {
 	"unblevable/quick-scope",
 	config = function()
 		-- Set custom highlights for QuickScope
-        -- local primary = vim.api.nvim_get_hl_by_name("Visual", true)
-        -- local secondary = vim.api.nvim_get_hl_by_name("Type", true)
+		-- local primary = vim.api.nvim_get_hl_by_name("Visual", true)
+		-- local secondary = vim.api.nvim_get_hl_by_name("Type", true)
 
-        -- vim.api.nvim_set_hl(0, 'QuickScopePrimary', primary)
-        -- vim.api.nvim_set_hl(0, 'QuickScopeSecondary', secondary)
+		-- vim.api.nvim_set_hl(0, 'QuickScopePrimary', primary)
+		-- vim.api.nvim_set_hl(0, 'QuickScopeSecondary', secondary)
 	end,
 }
