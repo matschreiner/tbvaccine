0e01701bcd627762ca0ac2337414f3ae6a5abc3d
Mathias Schreiner <matschreiner@gmail.com>
2024-02-23 10:38:33 +0100

    remove old functionality from telescope

--- configs/nvim/lua/plugins/telescope.lua
+++ configs/nvim/lua/plugins/telescope.lua
@@ -1,38 +1,3 @@
-local ts_select_dir_for_grep = function(_)
-	local action_state = require("telescope.actions.state")
-	local fb = require("telescope").extensions.file_browser
-	local live_grep = require("telescope.builtin").live_grep
-	local find_files = require("telescope.builtin").find_files
-	local current_line = action_state.get_current_line()
-
-	fb.file_browser({
-		files = false,
-		depth = false,
-		attach_mappings = function(_)
-			require("telescope.actions").select_default:replace(function()
-				local entry_path = action_state.get_selected_entry().Path
-				local dir = entry_path:is_dir() and entry_path or entry_path:parent()
-				local relative = dir:make_relative(vim.fn.getcwd())
-				local absolute = dir:absolute()
-
-				live_grep({
-					results_title = relative .. "/",
-					cwd = absolute,
-					default_text = current_line,
-				})
-
-				find_files({
-					results_title = relative .. "/",
-					cwd = absolute,
-					default_text = current_line,
-				})
-			end)
-
-			return true
-		end,
-	})
-end
-
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
@@ -43,29 +8,9 @@ return {
 	},
 	config = function()
 		local telescope = require("telescope")
-		local actions = require("telescope.actions")
 		telescope.setup({
 			defaults = {
 				path_display = { "truncate " },
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous, -- move to prev result
-						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-					},
-				},
-			},
-			pickers = {
-				live_grep = {
-					mappings = {
-						i = {
-							["<C-f>"] = ts_select_dir_for_grep,
-						},
-						n = {
-							["<C-f>"] = ts_select_dir_for_grep,
-						},
-					},
-				},
 			},
 		})
 		telescope.load_extension("fzf")
