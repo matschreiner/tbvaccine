cefa651db4c4f9059742942af9c5883284d03362
Mathias Schreiner <matschreiner@gmail.com>
2024-02-24 13:15:07 +0100

    add slime

---
+++ configs/nvim/lua/plugins/vim-slime.lua
@@ -0,0 +1,11 @@
+return {
+    'jpalardy/vim-slime',
+    config = function()
+        vim.g.slime_target = "tmux"
+        vim.g.slime_default_config = {
+            socket_name = "default",
+            target_pane = "1"
+        }
+    end
+}
+
