432ef71a2328d8f697fef1fb7ac02cf1dce06e88
Mathias Schreiner <matschreiner@gmail.com>
2024-02-28 12:37:34 +0100

    styula

--- configs/nvim/lua/plugins/ultisnips.lua
+++ configs/nvim/lua/plugins/ultisnips.lua
@@ -1,11 +1,11 @@
 return {
-	"SirVer/ultisnips",
-	config = function()
-		vim.g.UltiSnipsExpandTrigger = "<c-e>"
-		vim.g.UltiSnipsJumpForwardTrigger = "<leader>j"
-		vim.g.UltiSnipsJumpBackwardTrigger = "<leader>k"
+    "SirVer/ultisnips",
+    config = function()
+        vim.g.UltiSnipsExpandTrigger = "<c-e>"
+        vim.g.UltiSnipsJumpForwardTrigger = "<leader>j"
+        vim.g.UltiSnipsJumpBackwardTrigger = "<leader>k"
 
-		vim.keymap.set("n", "<leader>u", ":UltiSnipsEdit<CR>")
-		-- vim.g.ultisnips_snippet_directories = { "$HOME/.configs/nvim/snips/" }
-	end,
+        vim.keymap.set("n", "<leader>u", ":UltiSnipsEdit<CR>")
+        -- vim.g.ultisnips_snippet_directories = { "$HOME/.configs/nvim/snips/" }
+    end,
 }
