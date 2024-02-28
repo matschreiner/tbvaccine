3b908007ae59b23c63b6be92c18e47940b07a4b0
Mathias Schreiner <matschreiner@gmail.com>
2024-02-28 17:11:18 +0100

    add run vimsluime

--- configs/nvim/lua/plugins/vim-slime.lua
+++ configs/nvim/lua/plugins/vim-slime.lua
@@ -1,10 +1,11 @@
 return {
-	"jpalardy/vim-slime",
-	config = function()
-		vim.g.slime_target = "tmux"
-		vim.g.slime_default_config = {
-			socket_name = "default",
-			target_pane = "1",
-		}
-	end,
+    "jpalardy/vim-slime",
+    config = function()
+        vim.g.slime_target = "tmux"
+        vim.g.slime_default_config = {
+            socket_name = "default",
+            target_pane = "1",
+        }
+        vim.keymap.set("n", "<C-C><C-R>", ":execute 'SlimeSend1 python ' . shellescape(expand('%'))<CR>")
+    end,
 }
