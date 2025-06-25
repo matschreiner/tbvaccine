cf0a7a77ae4a97b7020888e0cc8b4b2ff44ac7ee
Mathias Schreiner <matschreiner@gmail.com>
2025-06-25 09:58:16 +0200

    longer lines

--- tbvaccine/tbv.py
+++ tbvaccine/tbv.py
@@ -24,9 +24,7 @@ class State:
 
 class TBVaccine:
     TB_END_RE = re.compile(r"^(?P<exception>[\w\.]+)\: (?P<description>.*?)$")
-    TB_FILE_RE = re.compile(
-        r'^  File "(?P<filename>.*?)", line (?P<line>\d+), in (?P<func>.*)$'
-    )
+    TB_FILE_RE = re.compile(r'^  File "(?P<filename>.*?)", line (?P<line>\d+), in (?P<func>.*)$')
     VAR_PREFIX = "|     "
 
     def __init__(self, code_dir=None, isolate=True, show_vars=False, max_length=120):
@@ -43,9 +41,7 @@ class TBVaccine:
         self._load_config()
 
         self.pygments_lexer = PythonLexer()
-        self.pygments_formatter = TerminalFormatter(
-            style=self._config.get("style", "color_scheme")
-        )
+        self.pygments_formatter = TerminalFormatter(style=self._config.get("style", "color_scheme"))
 
     def _load_config(self):
         dir_path = user_config_dir("tbvaccine")
@@ -69,9 +65,7 @@ class TBVaccine:
             # Check if there's an ANSI escape in the last few chars of max_length and break before it.
             if "\x1b" in short_text[-10:]:
                 short_text = short_text[: short_text.rfind("\x1b")]
-            text = short_text + "\x1b[0m ... ({} more chars)".format(
-                len(text) - len(short_text)
-            )
+            text = short_text + "\x1b[0m ... ({} more chars)".format(len(text) - len(short_text))
         if fg or style:
             styles = {"bright": 1, None: 0}
             colors = {
@@ -211,18 +205,14 @@ class TBVaccine:
 
             max_length = max([len(x[0]) for x in var_tuples])
             for key, val in var_tuples:
-                if type(val) in (type(lambda: None), type(sys)) or (
-                    key.startswith("__") and key.endswith("__")
-                ):
+                if type(val) in (type(lambda: None), type(sys)) or (key.startswith("__") and key.endswith("__")):
                     # We don't want to print functions or modules or __variables__.
                     continue
                 try:
                     val = str(val)
                 except:  # noqa
                     val = "<CANNOT CONVERT VALUE>"
-                lines.append(
-                    "%s%s = %s" % (self.VAR_PREFIX, key.ljust(max_length), val)
-                )
+                lines.append("%s%s = %s" % (self.VAR_PREFIX, key.ljust(max_length), val))
         lines.append("%s: %s" % (value.__class__.__name__, value))
         return "".join(self._process_line(line) for line in lines)
 
